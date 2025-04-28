3d59457f82d5c23bca60c86e6aa10e2a5e668236
jmrorihuela <jmrorihuela@gmail.com>
2025-04-27 20:55:47 -0600

    feature: 	contenido del blog 	header y footer

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -13,7 +13,6 @@ function Blog() {
     useEffect(() => {
         const postId = params.postId;
         const currentPost = postList.publicaciones.find((post) => post.id == postId);
-
         setPostData(currentPost);
     }, []);
 
@@ -28,6 +27,39 @@ function Blog() {
                         <li>{postData?.tiempo_lectura} min.</li> |
                         <li>{postData?.actualizado}</li>
                     </ul>
+                </div>
+            </div>
+
+            <div id="blogContent">
+                <div id="blogBody">
+                    <p className="blogText intro">
+                        {postData?.contenido?.introduccion}
+                    </p>
+
+                    <div id="graphicSection">
+                        <img className="blogGrafico" src={postData?.contenido?.grafico} alt="graficoAlt" />
+                        <span>| {postData?.contenido?.descripcion_grafico}</span>
+                    </div>
+
+                    <p className="blogText">
+                        {postData?.contenido?.cuerpo}
+                    </p>
+
+                    <div id="phraseSection">
+                        <h1>- {postData?.contenido?.frase}</h1>
+                        <small>{postData?.contenido?.autor_frase}.</small>
+                    </div>
+
+                    <p className="blogText">
+                        {postData?.contenido?.conclusion}
+                    </p>
+                    <small id="smallAuthor">{postData?.autor}, publicado el {postData?.actualizado}</small>
+                </div>
+
+                <div id="relatedPost">
+                    <div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
